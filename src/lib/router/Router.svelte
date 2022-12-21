90172efa4b28b198ef02ec5c4efa0a0bcfcf4355
Fractal-Tess <vgfractal@gmail.com>
2022-12-21 02:57:50 +0200

    add transition

--- src/lib/router/Router.svelte
+++ src/lib/router/Router.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
-  import { Route } from 'tinro';
+  import { Route, router } from 'tinro';
+  router.mode.hash();
 
   import Index from '$lib/router/routes/Index.svelte';
   import CallTauri from '$router/routes/CallTauri.svelte';
   import HashString from '$router/routes/HashString.svelte';
   import Versions from '$router/routes/Versions.svelte';
+  import Transition from '$lib/components/transition/Transition.svelte';
 </script>
 
 <!-- Fix for linting errors: ->
    https://github.com/AlexxNB/tinro/pull/121/commits/d2251ffed630aac6e76e71856204ead5dd2f6661 -->
-<Route path="/"><Index /></Route>
-<Route path="/call-tauri"><CallTauri /></Route>
-<Route path="/hash-string"><HashString /></Route>
-<Route path="/versions"><Versions /></Route>
+<Transition>
+  <Route path="/"><Index /></Route>
+  <Route path="/call-tauri"><CallTauri /></Route>
+  <Route path="/hash-string"><HashString /></Route>
+  <Route path="/versions"><Versions /></Route>
+</Transition>
