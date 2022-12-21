d36b08f0a28cfc4a57cb60daee3e80c60693f1fc
Fractal-Tess <vgfractal@gmail.com>
2022-12-21 02:57:31 +0200

    space

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,6 @@
 import type { IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 export const themes = ['dark', 'light'] as const;
-
 export type Theme = typeof themes[number];
 
 export type NavLink = {
