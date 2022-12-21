8fb587155d825f79e3cf8d11c01d36ace2e5cea8
Fractal-Tess <vgfractal@gmail.com>
2022-12-21 02:09:44 +0200

    new theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,26 +17,14 @@ const config = {
     themes: [
       {
         light: {
-          primary: '#24c8db',
-          secondary: '#f73c00',
-          accent: '#ffc131',
-          neutral: '#25343C',
-          'base-100': '#F8F8FB',
-          info: '#93BADC',
-          success: '#12A168',
-          warning: '#F4C857',
-          error: '#DE3B4E'
+          ...require('daisyui/src/colors/themes')['[data-theme=lofi]'],
+          primary: '#45B1E8',
+          secondary: '#fbbf24'
         },
         dark: {
-          primary: '#24c8db',
-          secondary: '#f73c00',
-          accent: '#ffc131',
-          neutral: '#2b2b2b',
-          'base-100': '#1f1f1f',
-          info: '#35B0F3',
-          success: '#1B743C',
-          warning: '#F38A12',
-          error: '#F2215C'
+          ...require('daisyui/src/colors/themes')['[data-theme=black]'],
+          primary: '#45B1E8',
+          secondary: '#fbbf24'
         }
       }
     ]
