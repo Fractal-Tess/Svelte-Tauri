37b13ed721a2b1f098fb77fd1abd0c793cb7dd98
Fractal-Tess <vgfractal@gmail.com>
2022-12-24 05:11:28 +0200

    example

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,12 +4,25 @@
 )]
 
 use sha2::{Digest, Sha256};
-use tauri::RunEvent;
+use tauri::{Manager, RunEvent};
+use webkit2gtk::{PermissionRequestExt, WebViewExt};
+use webkit2gtk_sys::request;
 
 fn main() {
     let app = tauri::Builder::default()
         .plugin(tauri_plugin_window_state::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![called_from_js, hash256sum])
+        .invoke_handler(tauri::generate_handler![called_from_js, hash256sum,])
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            window.with_webview(|webview| {
+                webview.inner().connect_permission_request(|_, request| {
+                    println!("{}", request.);
+                    request.allow();
+                    true
+                });
+            });
+            Ok(())
+        })
         .build(tauri::generate_context!())
         .expect("error while running tauri application");
 
--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,14 @@
   import { theme } from '$lib/stores/theme';
   import Router from '$lib/router/Router.svelte';
   import BaseLayout from '$lib/layout/BaseLayout.svelte';
-
+  
   $: {
     document.documentElement.setAttribute('data-theme', $theme);
     document.documentElement.classList.value = $theme;
   }
+
+  const geo = navigator.geolocation;
+  geo.getCurrentPosition(console.log);
 </script>
 
 <BaseLayout>
