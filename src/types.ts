ce0b809127badd1d6840820810159394cdae2e03
Fractal-Tess <vgfractal@gmail.com>
2023-05-12 23:29:16 +0300

    remove datalink type

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,2 @@
-import type { IconDefinition } from '@fortawesome/free-solid-svg-icons';
-
 export const themes = ['dark', 'light'] as const;
-export type Theme = typeof themes[number];
-
-export type NavLink = {
-  target: {
-    href: string;
-    newTab?: boolean;
-  };
-  content: {
-    text?: string;
-    icon?: IconDefinition;
-  };
-};
+export type Theme = (typeof themes)[number];
