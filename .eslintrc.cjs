14df58178e1e0bb90447d87644b264299c85ea9d
Fractal-Tess <vgfractal@gmail.com>
2024-01-24 04:07:03 +0200

    stop error on unused

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,25 +27,5 @@ module.exports = {
         parser: '@typescript-eslint/parser'
       }
     }
-  ],
-    rules: {
-    'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_'
-      }
-    ],
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        "argsIgnorePattern": "^_",
-        "varsIgnorePattern": "^\$\$(Props|Events|Slots|Generic)$"
-      }
-    ]
-  },
+  ]
 }
