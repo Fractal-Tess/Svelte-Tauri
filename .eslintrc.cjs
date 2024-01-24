e0602e5fbe61239d4287affbd36b71147672e279
Fractal-Tess <vgfractal@gmail.com>
2024-01-24 03:56:40 +0200

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,34 @@
+/** @type { import("eslint").Linter.FlatConfig } */
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:json/recommended',
+    'plugin:svelte/recommended',
     'prettier'
   ],
-  rules: {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte']
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    }
+  ],
+    rules: {
     'no-unused-vars': 'off', // or "@typescript-eslint/no-unused-vars": "off",
     'unused-imports/no-unused-imports': 'error',
     'unused-imports/no-unused-vars': [
@@ -27,19 +48,4 @@ module.exports = {
       }
     ]
   },
-  plugins: ['svelte3', '@typescript-eslint', 'unused-imports'],
-  ignorePatterns: ['*.cjs'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-  settings: {
-    'svelte3/typescript': () => require('typescript')
-  },
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2020
-  },
-  env: {
-    browser: true,
-    es2017: true,
-    node: true
-  }
-};
+}
